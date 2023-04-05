d464f2558dc4bab4b26bd902a309c0e060bad466
jbaudens <jbaudens@gmail.com>
2023-04-05 12:47:37 -0600

    Initial Commit

---
+++ CyclingBLESensorTester/CyclingBLESensorTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyclingBLESensorTesterApp.swift
+//  CyclingBLESensorTester
+//
+//  Created by JB Baudens on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CyclingBLESensorTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
